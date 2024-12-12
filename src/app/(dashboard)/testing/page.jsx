89f6dd7a5f374de1703efc8593e585ff446d3f54
Som Gupta <ssomvishwari786@gmail.com>
2024-12-12 15:58:32 +0530

    add testing screen

---
+++ src/app/(dashboard)/testing/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import ModelViewer from '@/components/ModelViewer/modelViewer'
+import ModelViewer from '@/components/ModelViewer/modelNew'
+const page = () => {
+  return (
+    <div>
+        <ModelViewer/>
+    </div>
+  )
+}
+
+export default page
